d5c065d06c894238c3175d19ab511d430aedf13c
nonepork <59335048+nonepork@users.noreply.github.com>
2024-09-15 02:56:49 +0800

    Update yolo-detection.py

--- yolo-detection.py
+++ yolo-detection.py
@@ -1,7 +1,7 @@
 import torch
 from ultralytics import YOLO
 
-camera1_index = 'rtsp://192.168.1.102:8080/h264_aac.sdp' # Input your camera
+camera1_index = '' # Input your camera
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = YOLO('yolov8n-pose.pt')
