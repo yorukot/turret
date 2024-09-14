e069cebdbd25400cedc02cd56f9768c04488940a
nonepork <59335048+nonepork@users.noreply.github.com>
2024-09-14 23:54:43 +0800

    Improved image detection code

--- yolo-detection.py
+++ yolo-detection.py
@@ -1,7 +1,10 @@
+import torch
 from ultralytics import YOLO
 
-camera1_index = '' # Input your camera
+camera1_index = 'rtsp://192.168.1.102:8080/h264_aac.sdp' # Input your camera
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = YOLO('yolov8n-pose.pt')
 
-results = model(source=camera1_index, stream=True, show=True, conf=0.3)
+for result in model(source=camera1_index, device=device, stream=True, show=True, conf=0.3):
+    print(result)
